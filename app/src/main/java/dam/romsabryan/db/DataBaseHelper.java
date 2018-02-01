19a683218388e41b48282c5e789849ad2616faeb
romsanbryan <romsanbryan@gmail.com>
2018-02-01 21:42:06 +0100

    Insert, Update and Delete values to SQLite Database table Show SQLite Database table Values

--- app/src/main/java/dam/romsabryan/db/DataBaseHelper.java
+++ app/src/main/java/dam/romsabryan/db/DataBaseHelper.java
@@ -1,38 +1,123 @@
 package dam.romsabryan.db;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 /**
- * Created by romsanbryan on 31/01/18.
+ * Clase operacional de la aplicacion.
+ * Contiene los metodos necesarios para crear, annadir, actualizar y borrar datos
+ *
+ * @author romsanbryan
+ * @version 1.0.0  2018-02-01
+ * @see SQLiteOpenHelper
  */
 
 public class DataBaseHelper extends SQLiteOpenHelper {
+
+    /**
+     * Constructor utilizado por la clase SQLiteOpenHelper para crear la base de datos
+     *
+     * @param context Contexto de la aplicacion
+     */
     public DataBaseHelper(Context context) {
         super(context, DATABASE_NAME, null, 1);
-        SQLiteDatabase db = this.getWritableDatabase();
     }
 
+    /**
+     * Crea la base de datos con el nombre de la tabla y los campos que necesitemos.
+     *
+     * @param db Base de datos
+     */
     @Override
     public void onCreate(SQLiteDatabase db) {
         db.execSQL("CREATE TABLE "+TABLE_NAME+" ("+COL_1+" INTEGER PRIMARY KEY AUTOINCREMENT,"
                 +COL_2+" TEXT,"+COL_3+" TEXT,"+COL_4+" INTEGER)");
     }
 
+    /**
+     *
+     * @param db Base de datos
+     * @param i
+     * @param i1
+     */
     @Override
     public void onUpgrade(SQLiteDatabase db, int i, int i1) {
         db.execSQL("DROP TABLE IF EXISTS "+TABLE_NAME);
         onCreate(db);
     }
 
+    /**
+     * Insertar datos en una tabla mediante un contenedor de valores (ContentView)
+     *
+     * @param name Nombre del estudiante
+     * @param subname Apellido del estudiante
+     * @param marks Nota del estudiante
+     * @return True si los datos se introduciron correctamente
+     */
+    public boolean insertData(String name, String subname, String marks){
+        SQLiteDatabase db = this.getWritableDatabase(); // Activamos que podemos escribir en la base de datos
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COL_2, name);
+        contentValues.put(COL_3, subname);
+        contentValues.put(COL_4, marks);
+        long result = db.insert(TABLE_NAME, null, contentValues);
+        if (result==-1)
+            return false;
+        else
+            return true;
+    }
+
+    /**
+     * Recorre la base de datos  con sentencia SQL
+     *
+     * @return Datos de la base de datos
+     */
+    public Cursor getAllData(){
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor res = db.rawQuery("SELECT * FROM "+TABLE_NAME, null);
+        return res;
+    }
+
+    /**
+     * Actualiza un registro de la base de datos segun el ID que le pasemos utilizando un contenido de
+     * valores (contentValue)
+     *
+     * @param id ID para actualizar
+     * @param name Nombre nuevo
+     * @param subname Apellido nuevo
+     * @param marks Nota nueva
+     * @return True
+     */
+    public boolean updateData(String id, String name, String subname, String marks){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COL_1, id);
+        contentValues.put(COL_2, name);
+        contentValues.put(COL_3, subname);
+        contentValues.put(COL_4, marks);
+        db.update(TABLE_NAME, contentValues, COL_1+" = ?", new String[] { id });
+        return true;
+    }
+
+    /**
+     * Borra un registro de la tabla en la base de datos segun el id
+     *
+     * @param id ID para borrar
+     * @return -1 si hay algun error con el delete
+     */
+    public Integer deleteData(String id){
+        SQLiteDatabase db = this.getWritableDatabase();
+        return db.delete(TABLE_NAME, COL_1+" = ?", new String[] { id });
+    }
+
     // Variables
-    public static final String DATABASE_NAME = "Student.db";
-    public static final String TABLE_NAME = "student_table";
-    public static final String COL_1 = "ID";
-    public static final String COL_2 = "Name";
-    public static final String COL_3= "Subname";
-    public static final String COL_4= "Marks";
-
-
+    public static final String DATABASE_NAME = "Student.db"; // DataBase Name
+    public static final String TABLE_NAME = "student_table"; // Table Name
+    public static final String COL_1 = "ID"; // Colum 1
+    public static final String COL_2 = "Name"; // Colum 2
+    public static final String COL_3= "Subname"; // Colum 3
+    public static final String COL_4= "Marks"; // Colum 4
 }
--- app/src/main/java/dam/romsabryan/db/MainActivity.java
+++ app/src/main/java/dam/romsabryan/db/MainActivity.java
@@ -1,15 +1,149 @@
 package dam.romsabryan.db;
 
+import android.database.Cursor;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+/**
+ * Aplicacion de una base de datos de "estudiantes" en dispositivo Android.
+ * En esta base de datos podemos realizar las operaciones de creacion escritura, lectura, actualizacion y borrado
+ *
+ * @author romsanbryan
+ * @version 1.0.0  2018-02-01
+ * @since API 15
+ */
 public class MainActivity extends AppCompatActivity {
-    DataBaseHelper myDB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        myDB = new DataBaseHelper(this);
+        myDB = new DataBaseHelper(this); // Definimos la clase pasandole el contexto
+
+        // Definicion de objetos
+        editName = (EditText) findViewById(R.id.editText_name); // Student Name
+        editSubname = (EditText) findViewById(R.id.editText_subname); // Student Subname
+        editMarks = (EditText) findViewById(R.id.editText_marks); // Student Marks
+        editID = (EditText) findViewById(R.id.editText_id); // Student ID
+
+        btnAddData = (Button) findViewById(R.id.button_add); // Button add
+        btnViewAll = (Button) findViewById(R.id.button_view); // Button view
+        btnUpdate = (Button) findViewById(R.id.button_update); // Button update
+        btnDel = (Button) findViewById(R.id.button_del); // Button delete
+
+        // Metodos
+        addData();
+        viewAll();
+        updateData();
+        del();
     }
+
+    /**
+     * Acción de boton para introducir datos. Si lo inserta correctamente manda un Toast con mensaje de informacion
+     * si no, manda con mensaje de datos no insertados
+     */
+    private void addData(){
+        btnAddData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                boolean  isInserted = myDB.insertData(editName.getText().toString(),
+                        editSubname.getText().toString(), editMarks.getText().toString());
+                if (isInserted == true)
+                    Toast.makeText(MainActivity.this, "Data Inserted", Toast.LENGTH_LONG).show();
+                else
+                    Toast.makeText(MainActivity.this, "Data not Inserted", Toast.LENGTH_LONG).show();
+
+            }
+        });
+    }
+
+    /**
+     * Acción de boton para visualizar datos.
+     */
+    private void viewAll(){
+        btnViewAll.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Cursor res = myDB.getAllData();
+                if (res.getCount() == 0){
+                    // show message
+                    showMessage("Error", "Nothing found");
+                    return;
+                }
+                StringBuffer buffer = new StringBuffer();
+                while (res.moveToNext()){
+                    buffer.append("ID: "+res.getString(0)+"\n");
+                    buffer.append("Name: "+res.getString(1)+"\n");
+                    buffer.append("Subname: "+res.getString(2)+"\n");
+                    buffer.append("Marks: "+res.getString(3)+"\n\n");
+
+                }
+                // Show all Data
+                showMessage("Data", buffer.toString());
+            }
+        });
+    }
+
+    /**
+     * Mensaje de AlertDialog para mostrar datos de la consulta
+     * @param title Titulo de la ventana, si no hay datos nos mostrara "error" y si los hay "data"
+     * @param message Mensaje de la ventana, muestra los datos de la base de datos si hubiera
+     */
+    private void showMessage(String title, String message){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setCancelable(true);
+        builder.setTitle(title);
+        builder.setMessage(message);
+        builder.show();
+    }
+
+    /**
+     * Accion del boton actualizar. Si lo actualiza correctamente manda un Toast con mensaje de informacion
+     * si no, manda con mensaje de datos no actualizados
+     */
+    private void updateData(){
+        btnUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                boolean isUpdate = myDB.updateData(editID.getText().toString(), editName.getText().toString(),
+                        editSubname.getText().toString(), editMarks.getText().toString());
+                if (isUpdate == true)
+                    Toast.makeText(MainActivity.this, "Data Update", Toast.LENGTH_LONG).show();
+                else
+                    Toast.makeText(MainActivity.this, "Data not Update", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    /**
+     * Accion del boton borrar. Si lo borra correctamente manda un Toast con mensaje de informacion
+     * si no, manda con mensaje de datos no borra
+     */
+    private void del(){
+        btnDel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Integer delRows = myDB.deleteData(editID.getText().toString());
+                if (delRows > 0)
+                    Toast.makeText(MainActivity.this, "Data Delete", Toast.LENGTH_LONG).show();
+                else
+                    Toast.makeText(MainActivity.this, "Data not Delte", Toast.LENGTH_LONG).show();
+
+            }
+        });
+    }
+
+    // Variables
+    private DataBaseHelper myDB; // Objeto de clase
+    private EditText editName, editSubname, editMarks, editID; // EditText para recoger valores
+        // Botones
+    private Button btnAddData; // Add
+    private Button btnViewAll; // View
+    private Button btnUpdate; // Update
+    private Button btnDel; // Delete
 }
